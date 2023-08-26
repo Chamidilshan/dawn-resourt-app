668fa02c51183dd3c5eea216e28bf345ca0e3809
Gosith Jayathilake <gosithnalan@gmail.com>
2023-08-26 13:50:57 +0530

    added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import { Component } from "react";
+import "./navbarstyle.css";
+import { MenuItems } from "./MenuItems";
+
+
+class Navbar extends Component{
+    state = {clicked:false};
+    handleclick=()=>{
+        this.setState({clicked:!this.state.clicked})
+    }
+    render(){
+        return(
+            <nav className="NavBarItems">
+                <h1 className="navbar-logo">Dawn Resorts</h1>
+                <div className="menu-icons">
+                    <i onClick={this.handleclick} className={this.state.clicked ? "fas fa-times":"fas fa-bars"}></i>
+                   
+                </div>
+                <ul className={this.state.clicked ? "nav-menu active":"nav-menu"}>
+                    {MenuItems.map((item,index)=>{
+                        return(
+                            <li key={index}><a className={item.cName} href="/"><i class={item.icon}></i>{item.title}</a></li>
+                        )
+                    })}
+                    
+                     
+                </ul>
+            </nav>
+        );
+    }
+}
+export default Navbar;

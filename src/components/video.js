a522a240fae9a4b4ebe0a2f78adfdb237a468abd
Gosith Jayathilake <gosithnalan@gmail.com>
2023-08-28 12:40:37 +0530

    finalized with video

--- src/components/video.js
+++ src/components/video.js
@@ -1,12 +1,12 @@
 import "./videoStyle.css";
-// import video from "../assets/videoplayback.mov";
+import video from "../assets/videoplayback.mov";
 
 function Videos(){
     return(
        <section className="video-area">
          <div className="videoholder">
             <h1>A glimpse of who we are</h1>
-            <video className="video" autoPlay  muted>
+            <video className="video" src={video} autoPlay  muted>
                 
             </video>
         </div>
